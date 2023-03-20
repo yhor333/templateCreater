840e0c5adb81a800712f87c99db4428ce82f4efb
Yan <yhor450@gmail.com>
2023-03-20 22:16:58 +0200

    set token in localStorage

---
+++ src/app/shared/instance/axios-instance/axios-instance.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const Cookies = require('js-cookie');
+
+export const instance = axios.create({
+  baseURL: 'http://185.235.218.150:8050/',
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(function (config) {
+  config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+  config.headers['Access-Control-Allow-Origin'] = '*';
+
+  return config;
+});
