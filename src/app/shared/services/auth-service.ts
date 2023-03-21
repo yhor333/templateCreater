e80f66ac4d8272bb865ddb8859a74a5688a1d8c4
Yan <yhor450@gmail.com>
2023-03-21 20:50:53 +0200

    add generete page,

--- src/app/shared/services/auth-service.ts
+++ src/app/shared/services/auth-service.ts
@@ -1,14 +1,10 @@
 import { instance } from '../instance/axios-instance/axios-instance';
-import { LoginFormData } from '../../shared-modules/auth-dialog/components/login-form/login-form';
-import { UserModel } from '../../shared-models/user-model';
 import { RegistrationFormData } from '../../shared-modules/auth-dialog/components/registration-form/registration-form';
-
-const Cookies = require('js-cookie');
 
 const AuthService = () => ({
   login: async (
     data: URLSearchParams,
-    loadUser: () => void,
+    // loadUser: () => void,
     setIsLoading: (isLoading: boolean) => void
   ): Promise<void> => {
     try {
@@ -16,7 +12,7 @@ const AuthService = () => ({
         localStorage.setItem('token', response.data.access_token);
       });
 
-      loadUser();
+      // loadUser();
       setIsLoading(true);
     } catch (e) {
       console.error(e);
@@ -25,22 +21,13 @@ const AuthService = () => ({
 
   registration: async (
     data: RegistrationFormData,
-    loadUser: () => void,
+    // loadUser: () => void,
     setIsLoading: (isLoading: boolean) => void
   ): Promise<void> => {
     try {
       await instance.post('/auth/registration', data);
-      loadUser();
+      // loadUser();
       setIsLoading(true);
-    } catch (e) {
-      console.error(e);
-    }
-  },
-
-  logout: (setUser: (user: UserModel | null) => void): void => {
-    try {
-      Cookies.remove('accessToken');
-      setUser(null);
     } catch (e) {
       console.error(e);
     }
